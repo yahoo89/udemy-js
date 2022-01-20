37bf4fe272dc8cef435715b0b0bcef9a11f05e7c
Tigran <tigran2589@gmail.com>
2022-01-20 19:10:25 +0200

    The first object

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 "use strict"
 
-console.log('work');
+const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const
+  a = prompt('One of the last movie ?'),
+  b = prompt('How would you rate the movie ?'),
+  c = prompt('One of the last movie ?'),
+  d = prompt('How would you rate the movie ?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
